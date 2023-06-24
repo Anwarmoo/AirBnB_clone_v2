06d8db554949df035a8d2fd9ef347ac31aa3ecce
MOHAMED ADAN <anwarmoh389@gmail.com>
2023-06-24 20:27:16 +0000

    0x02. AirBnB clone

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
     """Amenity class that inherits from BaseModel and Base"""
-    __tablename__ = "amenities"
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
